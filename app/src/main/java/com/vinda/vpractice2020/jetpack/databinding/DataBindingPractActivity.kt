4c15b1b107d7c8090a5b7d79d074a3799e4f834a
yyfd2913zy <52biangbiangmian>
2020-12-10 14:18:04 +0800

    测试代码

---
+++ app/src/main/java/com/vinda/vpractice2020/jetpack/databinding/DataBindingPractActivity.kt
@@ -0,0 +1,21 @@
+package com.vinda.vpractice2020.jetpack.databinding
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.vinda.vpractice2020.R
+
+class DataBindingPractActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+    }
+
+}
